5fa01dd74f3f9a86afe85952ae2c9f2d5ff6b42c
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-10-30 13:49:59 +0800

    added service-worker script

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("push", (event) =>{
+  const notification = event.data.json();
+
+  // eslint-disable-next-line no-restricted-globals
+  event.waitUntil(self.registration.showNotification(notification.title, {
+    body: notification.body,
+    icon: "./coclogo.jpg",
+    data: {
+      notifURL: notification.url
+    }
+  }));
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("notificationclick", (event) =>{
+  // eslint-disable-next-line no-undef
+  event.waitUntil(clients.openWindow(event.notification.data.notifURL));
+});
